b07d988642ee421d7e839f74569bb23361016078
Randy Conrad <ekgcorp@gmail.com>
2016-11-04 03:41:57 +0000

    Initial Form Design

---
+++ StoredValues/StoredValues/AafesStoredValueForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoredValues
+{
+    public partial class AafesStoredValueForm : Form
+    {
+        public AafesStoredValueForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AafesStoredValueForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
