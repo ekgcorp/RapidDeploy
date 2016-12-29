0946e2bbb91101bbcf12cb188040376973c40cda
Randy Conrad <ekgcorp@gmail.com>
2016-12-29 20:42:48 +0000

    Modified Help File

---
+++ RapidDeploy/RapidDeploy_REMOVE/Views/DevicePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RapidDeploy.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace RapidDeploy.Views
+{
+    public sealed partial class DevicePage : Page
+    {
+        public DevicePage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
